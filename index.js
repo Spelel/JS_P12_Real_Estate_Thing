0177ca390e059f3ef8aa525d39875d6695aa159a
Arek <arek-karczo.wp.pl@wp.pl>
2025-01-06 14:35:29 +0100

    none

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
-function getPropertyHtml() {
+import { placeholderPropertyObj as PPO} from "./properties/placeholderPropertyObj.js"
+import { propertyForSaleArr as PFSarr } from "./properties/propertyForSaleArr.js"
 
+
+function getPropertyHtml() {
+    let feedHtml =``
+
+    PFSarr.forEach(function (line) {
+        if (line.priceGBP > 0) {
+
+        }feedHtml +=`
+        <section class="card">
+        <img src="/images/${line.image}">
+            <div class="card-right">
+                <h2>${line.propertyLocation}</h2>
+                <h3>£${line.priceGBP}</h3>
+                <p>${line.comment}</p>
+                <h3>${line.roomsM2.reduce(function(total, value){
+                    return total + value
+                })} m&sup2;</h3>
+            </div>
+        </section> 
+        `
+    })
+    return feedHtml
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById('container').innerHTML = getPropertyHtml()
+
+
+
+
+
+
+
